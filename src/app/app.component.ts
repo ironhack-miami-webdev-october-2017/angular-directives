2ad5163daa176a009ca90dc3803d383d83a2a604
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2017-12-04 16:02:09 -0500

    Add *ngStyle example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  textColor: string = 'indigo';
+
+  ngOnInit() {
+      setInterval(
+        () => {
+            this.textColor = this.getRandomColor();
+        },
+        7000
+      );
+  }
+
+  getRandomColor() {
+      // random integers in the 0-255 range for rgb
+      const r = Math.floor(Math.random() * 256);
+      const g = Math.floor(Math.random() * 256);
+      const b = Math.floor(Math.random() * 256);
+
+      return `rgb(${r}, ${g}, ${b})`;
+  }
 }
